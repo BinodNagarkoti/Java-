5c88dfed968f1971f1400ccba1070a2dbec1286a
Binod Nagarkoti <binod1365@gmail.com>
2017-07-31 16:51:29 +0545

    Add files via upload

---
+++ helperclass/com/example/HelperClass.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+public class HelperClass{
+
+public static void main(String[] helperclass){
+ 	double doubleValue= 123.d;
+	System.out.println(doubleValue);
+
+}
+}
---
+++ vector/com/edu/vector.java
@@ -0,0 +1,14 @@
+package com.edu.binodnagarkoti
+import java.util.Vector;
+import java.util.Scanner;
+public class vector{
+	public ststic void main(String [] args){
+	Vector myVector =new new Vector();
+	myVector`````````````````````````````````````````````````````````
+CMUAB-D7USG-AXFMH-QG6I6-VA3OT-ECFLR
+	}
+
+uuuep4elgktik5oip[azai}
+
+
+];x4-2-ot
